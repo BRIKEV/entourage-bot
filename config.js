3b80e3e8c4bbc8feafaa0de8084a0faf3993c547
bri06 <briammartinez@gmail.com>
2021-05-15 23:38:51 +0200

    :wrench: added config file

---
+++ config.js
@@ -0,0 +1,15 @@
+const prefix = '!'
+
+const commands = {
+  "DEFAULT_COMMAND": "puto",
+  "WARZONE_COMMAND": "warson",
+  "JAVA_COMMAND": "java",
+  "SHOW_ALL": "commands",
+  "ADD_PUTO": "addputo",
+  "TODOS": "todos"
+}
+
+module.exports = {
+  prefix,
+  commands,
+}
